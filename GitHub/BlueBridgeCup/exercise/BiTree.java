b4c8cb8dfc597f6e10f0b3b71d7ffc808ae04fb2
xudan <xudaaaaan@163.com>
2017-04-02 22:32:20 +0800

    add tree

--- GitHub/BlueBridgeCup/exercise/BiTree.java
+++ GitHub/BlueBridgeCup/exercise/BiTree.java
@@ -1,4 +1,27 @@
-
+//搜索树
 public class BiTree {
+	protected int v;
+	protected BiTree l;
+	protected BiTree r;
+	
+	public BiTree(int v){
+		this.v = v;
+	}
+	
+	public void add(BiTree the){
+		if(the.v < v){
+			if(l == null){l = the;}
+				else{l.add(the);}
+		}else{
+			if(r == null){r = the;}
+				else{r.add(the);}
+		}
+	}
+	
+	public void mid_trav(){
+		l.mid_trav();
+		System.out.println(v);
+		r.mid_trav();
+	}
 
 }
